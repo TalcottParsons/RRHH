069296f5e2988ac362cd363e21eb60a14219f43e
TalcottParsons <ia.ch100423@ufg.edu.sv>
2024-12-01 10:52:11 -0600

    prueba

--- RRHH4/Program.cs
+++ RRHH4/Program.cs
@@ -8,7 +8,7 @@ namespace RRHH
 {
     internal static class Program
     {
-        /// <summary>
+        /// <summary> //pruebon
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
